dccacb9646c9dc1b6b9bed223c44ee65ebeb6683
Tom Raja <rajatamas@hotmail.com>
2020-03-21 01:27:31 +0100

    build

---
+++ build/precache-manifest.dfb6aa0da03f051a9a2d768f49e396e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da7f402c58e5fa391fbcb9280b25b4d9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "db7d58ff4a2b70a074ea",
+    "url": "./static/css/main.73694ffe.chunk.css"
+  },
+  {
+    "revision": "ae81177c1b2eca9b5d6c",
+    "url": "./static/js/2.780dacbb.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "./static/js/2.780dacbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db7d58ff4a2b70a074ea",
+    "url": "./static/js/main.18677f41.chunk.js"
+  },
+  {
+    "revision": "2b355d3cd18dcb51d4d6",
+    "url": "./static/js/runtime-main.a3282279.js"
+  }
+]);
